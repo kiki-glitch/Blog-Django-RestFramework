374db638de1af0da3f8a6c6b4326567774671d5d
unknown <kikialex37@gmail.com>
2025-07-26 19:55:11 +0300

    Defining the Blog Model & Serializer

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.utils.text import slugify
+from django.utils import timezone
+
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -12,3 +16,43 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Blog(models.Model): 
+
+    CATEGORY = (("Frontend", "Frontend"),
+                ("Backend", "Backend"),
+                ("Fullstack", "Fullstack"),
+                ("Design", "Design"),
+                ("Blockchain", "Blockchain")
+                )
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name="blogs", null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+    is_draft = models.BooleanField(default=True)
+    category = models.CharField(max_length=255, choices=CATEGORY, blank=True, null=True)
+    featured_image = models.ImageField(upload_to="blank_img", blank=True, null=True)
+
+    class Meta:
+        ordering = ["-published_time"]
+
+        def __str__(self):
+            return self.title
+        
+        def save(self, *args, **kwargs):
+            base_slug = slugify(self.title)
+            slug = base_slug
+            num = 1
+            while Blog.objects.filter(slug = slug).exists():
+                slug = f'{base_slug}-{num}'
+                num += 1
+            self.slug = slug
+
+            if not self.is_draft and self.published_date is None:
+                self.published_date = timezone.now()
+            
+            super().save(*args, **kwargs)
--- blogapp/serializers.py
+++ blogapp/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from .models import Blog
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,4 +23,9 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         new_user.set_password(password)
         new_user.save()
         return new_user
+    
+class BlogSeriaizer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = ields = ['id', 'title', 'slug', 'author', 'category', 'content', 'featured_image', 'published_date', 'created_at', 'updated_at', 'is_draft']
         
